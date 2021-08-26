3fd6011c7f290ecd645e7ab12598fdbb4a01ef50
DorjaSkenderovic <dorjaskenderovic@gmail.com>
2021-08-26 15:08:31 +0200

    Heroku

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,12 @@
-export const TIP_RACUNA_URL = 'http://localhost:8083/tipRacuna';
+/*export const TIP_RACUNA_URL = 'http://localhost:8083/tipRacuna';
 export const KREDIT_URL = 'http://localhost:8083/kredit';
 export const KLIJENT_URL = 'http://localhost:8083/klijent';
 export const RACUN_URL = 'http://localhost:8083/racun';
 export const RACUN_KLIJENT_URL = 'http://localhost:8083/klijentId';
+
+*/
+export const TIP_RACUNA_URL = 'https://backend-rva-project.herokuapp.com/tipRacuna';
+export const KREDIT_URL = 'https://backend-rva-project.herokuapp.com/kredit';
+export const KLIJENT_URL = 'https://backend-rva-project.herokuapp.com/klijent';
+export const RACUN_URL = 'https://backend-rva-project.herokuapp.com/racun';
+export const RACUN_KLIJENT_URL = 'https://backend-rva-project.herokuapp.com/klijentId';
